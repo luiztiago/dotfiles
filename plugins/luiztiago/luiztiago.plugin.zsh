637ff9d4425e816c925eb20f19caaf884a40c809
luiztiago <luiztiago@gmail.com>
2012-09-10 18:13:38 -0300

    shortcuts and luiztiago plugin

---
+++ plugins/luiztiago/luiztiago.plugin.zsh
@@ -0,0 +1,18 @@
+#
+# General
+#
+alias c=clear
+alias h=history
+alias g=git
+alias showhiddenfiles='defaults write com.apple.finder AppleShowAllFiles TRUE'
+alias hiddenfiles='defaults write com.apple.finder AppleShowAllFiles FALSE'
+alias killfinder='killall Finder'
+alias textedit='sudo /Applications/TextEdit.app/Contents/MacOS/TextEdit'
+alias firefox3='/Applications/Firefox3.app/Contents/MacOS/firefox-bin --profile profile'
+alias atools='cd ~/Mobile/_SDK/android-sdk-mac_x86/platform-tools/'
+
+#
+# Dev
+#
+alias htdocs='cd /usr/local/zend/apache2/htdocs/'
+alias cahier='cd /usr/local/zend/apache2/htdocs/mgr/cahier/'
